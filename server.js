10ae70c947f8a9b02c88261b1a213bb56216ff6c
Canberkkmn <canberkkaman@gmail.com>
2025-01-12 17:19:29 +0300

    add users schema and query

--- server.js
+++ server.js
@@ -6,13 +6,34 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
     type Query {
         hello: String
+        users: [User]
+    }
+
+    type User {
+        id: ID
+        name: String
+        email: String
     }
 `;
+
+const usersData = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'john@example.com'
+    },
+    {
+        id: 2,
+        name: 'Jane',
+        email: 'jane@example.com'
+    }
+];
 
 // Resolvers
 const resolvers = {
     Query: {
         hello: () => 'Hello world!',
+        users: () => usersData,
     },
 };
 
